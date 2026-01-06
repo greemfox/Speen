e59e738333103d17e8639c236e7ca46986e51a5b
greemfox <pavelkurchanov.lpk@gmail.com>
2026-01-06 21:01:10 +0700

    first commit

---
+++ main.lua
@@ -0,0 +1,8 @@
+-- TODO:
+-- [ ] normalize coords
+-- [ ] draw points
+-- [ ] connect points
+-- [ ] repeat
+function love.draw()
+    love.graphics.draw(love.graphics.newImage('^^.png'), 420, 69)
+end
