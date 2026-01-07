474be3ad5a1026d3236176c55d8b776edf7b4ae6
greemfox <pavelkurchanov.lpk@gmail.com>
2026-01-07 09:05:11 +0700

    There's so much left to do in this place. But we've made a start, haven't we, my friend? We've made a start.

--- main.lua
+++ main.lua
@@ -1,8 +1,20 @@
 -- TODO:
--- [ ] normalize coords
+-- [x] normalize coords
+---- [ ] center the drawing
 -- [ ] draw points
 -- [ ] connect points
 -- [ ] repeat
+width, height = love.graphics.getDimensions()
+function drawNormalized(normalizedX, normalizedY, imgPath, scale)
+    imgPath = imgPath or '^^.png'
+    scale = scale or 0.1
+    x = (normalizedX + 1) * (width / 2)
+    y = (normalizedY - 1) * -(height / 2)
+    love.graphics.draw(love.graphics.newImage(imgPath), x, y, 0, scale) -- 0 rads rotation
+end
+
 function love.draw()
-    love.graphics.draw(love.graphics.newImage('^^.png'), 420, 69)
+    for i = -1, 1, 0.01 do
+        drawNormalized(i, i)
+    end
 end
