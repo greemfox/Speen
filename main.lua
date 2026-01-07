ef83be83efa4e036523feb3ebb605ef4a37fdbb4
greemfox <pavelkurchanov.lpk@gmail.com>
2026-01-07 11:49:15 +0700

    piano piano si va lontano

--- main.lua
+++ main.lua
@@ -1,16 +1,18 @@
 -- TODO:
 -- [x] normalize coords
----- [ ] center the drawing
+---- [x] center the drawing
 -- [ ] draw points
 -- [ ] connect points
 -- [ ] repeat
 width, height = love.graphics.getDimensions()
 function drawNormalized(normalizedX, normalizedY, imgPath, scale)
-    imgPath = imgPath or '^^.png'
-    scale = scale or 0.1
-    x = (normalizedX + 1) * (width / 2)
-    y = (normalizedY - 1) * -(height / 2)
-    love.graphics.draw(love.graphics.newImage(imgPath), x, y, 0, scale) -- 0 rads rotation
+    local imgPath = imgPath or '^^.png'
+    local scale = scale or 0.1
+    local img = love.graphics.newImage(imgPath)
+    local imgWidth, imgHeight = img:getDimensions()
+    local x = (normalizedX + 1) * (width / 2) - (imgWidth * scale / 2)
+    local y = (normalizedY - 1) * (-height / 2) - (imgHeight * scale / 2)
+    love.graphics.draw(img, x, y, 0, scale) -- 0 rads rotation
 end
 
 function love.draw()
