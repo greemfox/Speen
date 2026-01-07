591c9711912d5301d901e9a9a0d5d6d674986ed6
greemfox <pavelkurchanov.lpk@gmail.com>
2026-01-07 12:25:35 +0700

    make sure all's running smoothly on the 2d end of things

--- main.lua
+++ main.lua
@@ -1,18 +1,22 @@
 -- TODO:
--- [ ] draw points
+-- [ ] project from 3d
 -- [ ] connect points
--- [ ] repeat
-width, height = love.graphics.getDimensions()
+-- [ ] redraw
+winWidth, winHeight = love.graphics.getDimensions()
 function drawNormalized(normalizedX, normalizedY, scale, imgPath)
     local scale = scale or 0.1
     local imgPath = imgPath or '^^.png'
     local img = love.graphics.newImage(imgPath)
     local imgWidth, imgHeight = img:getDimensions()
-    local x = (normalizedX + 1) * (width / 2) - (imgWidth * scale / 2)
-    local y = (normalizedY - 1) * (-height / 2) - (imgHeight * scale / 2)
+    local x = (normalizedX + 1) * (winWidth / 2) - (imgWidth * scale / 2)
+    local y = (normalizedY - 1) * (-winHeight / 2) - (imgHeight * scale / 2)
     love.graphics.draw(img, x, y, 0, scale) -- 0 rads rotation
 end
 
+local points = { { -0.5, 0.5 }, { 0.5, 0.5 }, { -0.5, -0.5 }, { 0.5, -0.5 } }
+
 function love.draw()
-    drawNormalized(0, 0)
+    for i = 1, #points, 1 do
+        drawNormalized(points[i][1], points[i][2])
+    end
 end
